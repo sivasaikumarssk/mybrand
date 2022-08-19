8b9c5d6f068b4854036d25f2cc6ea543519b0d83
sivasaikumar <sivasaikumar.pinjala@gmail.com>
2022-08-19 23:36:03 +0530

    vite installed for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+        <h1>Brand-and-Beyondit</h1>
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
